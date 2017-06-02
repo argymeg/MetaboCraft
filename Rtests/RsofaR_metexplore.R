d1e7b80f370ffd8605e5c5b1081a812bffb5b8a0
argymeg <argymeg@gmail.com>
2017-06-02 13:10:51 +0100

    Duplicate side compounds. Very strict filtering, only consider sources.

--- Rtests/RsofaR_metexplore.R
+++ Rtests/RsofaR_metexplore.R
@@ -9,7 +9,28 @@ inputFile = "1755sterol.json"
 #TODO1: differentiate between metabolites and reactions
 #TODO2: experiment with graphing algorithms. Drl looks promising for large networks but let's get there first.
 graphData <- fromJSON(txt = inputFile)
-graph <- graph_from_data_frame(as.data.frame(cbind(graphData$links$source, graphData$links$target)), directed = FALSE)
+allLinks <- as.data.frame(cbind(graphData$links$source, graphData$links$target))
+colnames(allLinks) <- c("source","target")
+allNodes <- as.data.frame(cbind(c(0:(length(graphData$nodes$name)-1)),graphData$nodes$name,graphData$nodes$biologicalType), stringsAsFactors = FALSE)
+colnames(allNodes) <- c("localID","chemName","biologicalType")
+
+for(i in 0:(length(allNodes$localID)-1)){
+  #print(paste(i,sum(allLinks$source == i),sep = " sep "))
+  if(sum(allLinks$source == i) > 1 && allNodes[i+1,]$biologicalType == "metabolite"){
+    #print(paste(i,allNodes[i+1,]$chemName,sum(allLinks$source == i),sep = " sep "))
+    for(j in which(allLinks$source == i)[2:sum(allLinks$source == i)]){
+      #print(allNodes[i+1,])
+      allNodes[length(allNodes$localID)+1,] <- allNodes[i+1,]
+      #allNodes$localID <- as.character(allNodes$localID)
+      allNodes[length(allNodes$localID),]$localID <- length(allNodes$localID) - 1
+      #allNodes$localID <- as.factor(allNodes$localID)
+      allLinks[j,]$source <- allNodes[length(allNodes$localID),]$localID
+    }
+  }
+}
+sum(graphData$links$source == 28)
+
+graph <- graph_from_data_frame(allLinks, directed = FALSE)
 lo <- layout_(graph, with_fr(dim = 3),normalize(xmin=0,xmax=100))
 
 
@@ -28,8 +49,8 @@ if(min(lo) < 0) {
 nodesout <- as.data.frame(cbind((as_data_frame(graph, what ="vertices")$name),lo[,1],lo[,2],lo[,3]))
 colnames(nodesout) <- c("name", "x", "y", "z")
 for(i in 1:length(nodesout$name)){
-  nodesout$chemName[i] <- graphData$nodes$name[as.integer(as.character(nodesout$name[i]))+1]
-  nodesout$biologicalType[i] <- graphData$nodes$biologicalType[as.integer(as.character(nodesout$name[i]))+1]
+  nodesout$chemName[i] <- as.character(allNodes$chemName[as.integer(as.character(nodesout$name[i]))+1])
+  nodesout$biologicalType[i] <- as.character(allNodes$biologicalType[as.integer(as.character(nodesout$name[i]))+1])
 }
 nodesout <- nodesout[order(as.integer(as.character(nodesout$name))),]
 
