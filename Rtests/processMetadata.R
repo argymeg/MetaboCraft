5b7c3999523c19fe5fffd658a36a117378c0a002
argymeg <argymeg@gmail.com>
2017-06-08 13:53:47 +0100

    Record changing nodes in separate json file

--- Rtests/processMetadata.R
+++ Rtests/processMetadata.R
@@ -25,3 +25,28 @@ for (i in which(nodesout$biologicalType == "metabolite")){
     transCounter <- transCounter + 1
   }, error = function(e){print(e)})
 }
+
+
+#Import sample data and calculate a crude metric of positive or negative change (more than 2-fold)
+sampleData <- read.csv("pathos_KEGGlist2.csv")
+sampleFrame <- as.data.frame(cbind(sampleData$Mean..24hrs_A, sampleData$Mean..24hrs_B))
+rownames(sampleFrame) <- sampleData$C14
+colnames(sampleFrame) <- c("A-24", "B-24")
+sampleFrame$l2fc <- log2(sampleFrame$`B-24`/sampleFrame$`A-24`)
+sampleFrame$pos <- sampleFrame$l2fc > 1
+sampleFrame$neg <- sampleFrame$l2fc < -1
+
+#Create new data frame containing only node identifiers and positive or negative change
+#TODO1: Further condense output
+#TODO2: Use [] notation instead of subset
+transList <- as.list(transOut$result)
+names(transList) <- transOut$searchTerm
+nodeChange <- subset(nodesout, biologicalType == "metabolite", select = c("localID", "chemName"))
+nodeChange$KEGG <- as.character(transList[nodeChange$chemName])
+nodeChange <- subset(nodeChange, KEGG != "NULL")
+nodeChange$pos <- sampleFrame[nodeChange$KEGG,]$pos
+nodeChange$neg <- sampleFrame[nodeChange$KEGG,]$neg
+nodeChange <- subset(nodeChange, !is.na(pos))
+nodeChange <- subset(nodeChange, pos | neg)
+
+write_json((change = nodeChange), "outOfR_change.json", pretty = TRUE)
