15525dcc43811d483efa9cfc2b23efb6ae2f85b8
argymeg <argymeg@gmail.com>
2017-06-21 11:23:21 +0100

    Added conditions to click event

--- plugins/pimpcraft/myNodeClickHook.js
+++ plugins/pimpcraft/myNodeClickHook.js
@@ -1,13 +1,18 @@
 //Builds the graph of a pathway selected in the map
-//Currently throws multiple exceptions whenever any other block is clicked
-//Many checks to be added before this fires
+
 function myNodeClickHook(event){
   var player = event.player;
-  var location = event.getClickedBlock().getLocation();
-  //echo(breaker, location);
-  var selection = location.world.getNearbyEntities(location, 2, 2, 2)[0].getCustomName();
-  selection = selection.replace(/ /g, "%20")
-  var d = new Drone(player);
-  d.pullFromRAndBuildThis(selection);
+  if(event.getAction() == 'RIGHT_CLICK_BLOCK'){
+    if(event.getClickedBlock().getType() == 'REDSTONE_BLOCK'){
+      if(event.getHand() == 'HAND'){
+        var location = event.getClickedBlock().getLocation();
+        var selection = location.world.getNearbyEntities(location, 2, 2, 2)[0].getCustomName();
+        selection = selection.replace(/ /g, "%20");
+
+        var d = new Drone(player);
+        d.pullFromRAndBuildThis(selection);
+      }
+    }
+  }
 }
 events.playerInteract(myNodeClickHook);
