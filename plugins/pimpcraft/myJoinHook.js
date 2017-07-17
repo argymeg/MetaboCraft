96cb25180c62954e25539d2cdfb30d05a408a329
argymeg <argymeg@gmail.com>
2017-07-17 12:43:16 +0100

    Expanded joinhook to show player uploaded files

--- plugins/pimpcraft/myJoinHook.js
+++ plugins/pimpcraft/myJoinHook.js
@@ -1,15 +1,32 @@
 //Fires when a player logs in, welcoming and initialising
 
 var store = require('storage');
+var http = require('http');
+var player, playerFiles;
 
 function myJoinHook(event){
-  console.log("Hello world!");
-  var player = event.player;
+  player = event.player;
+  var playerFileSource = 'http://localhost:32908/listplayerfiles?player=' + player.name;
   store[player.name] = {};
   store[player.name]['bioSource'] = 4324;
-  echo(player, "Welcome to PiMPCraft, " + player.name + "!")
-  echo(player, "You are seeing BioSource " + store[player.name]['bioSource'] + ".")
-  console.log("Goodbye world!");
+
+  http.request(playerFileSource,
+  function(responseCode, responseBody){
+    playerFiles = JSON.parse(responseBody);
+    store[player.name]['currentFile'] = playerFiles[0];
+    console.log(store[player.name]['currentFile']);
+    showGreeting();
+  });
+}
+
+function showGreeting(){
+  echo(player, "Welcome to PiMPCraft, " + player.name + "!");
+  echo(player, "You are seeing BioSource " + store[player.name]['bioSource'] + ".");
+  echo(player, "Your currently available files are:");
+  for(var i = 0; i < playerFiles.length; i++){
+    echo(player, playerFiles[i]);
+  }
+  echo(player, "Your currently selected file is " + store[player.name]['currentFile']);
 }
 
 events.playerJoin(myJoinHook);
