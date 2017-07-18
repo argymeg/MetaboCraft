c2366ace498036adc41fba28ae5286bc823f862e
argymeg <argymeg@gmail.com>
2017-07-18 18:32:27 +0100

    New path fixes

--- Rscripts/plumberFunctions.R
+++ Rscripts/plumberFunctions.R
@@ -19,11 +19,11 @@ function(biosource, pathname){
 #* @get /listplayerfiles
 function(player){
   #return(list.files(pattern = paste0("^", player)))
-  return(list.files(path = "~/pimpcraft_working/data/", pattern = paste0("^outOfR_change_", player)))
+  return(list.files(path = "../cache/", pattern = paste0("^changeData_", player)))
 }
 
 #* @get /getplayerfile
 function(file){
   library(jsonlite)
-  return(fromJSON(paste0("~/pimpcraft_working/data/", file)))
+  return(fromJSON(paste0("../cache/", file)))
 }
