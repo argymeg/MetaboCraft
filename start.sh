3dec8dc91e552ed1f2f539c8d57bd34f6a35a4af
argymeg <argymeg@gmail.com>
2017-07-18 13:16:36 +0100

    Made start and stop scripts more user-friendly

--- start.sh
+++ start.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 cd spigot
-screen -dmS forSpigot
-screen -S forSpigot -p 0 -X stuff "cd spigot
+screen -dmS pimpcraftSpigot
+screen -S pimpcraftSpigot -p 0 -X stuff "cd spigot
 "
-screen -S forSpigot -p 0 -X stuff "exec java -jar $(ls | grep spigot- )
+screen -S pimpcraftSpigot -p 0 -X stuff "exec java -jar $(ls | grep spigot- )
 "
-echo done1
+
 cd ../Rscripts
-screen -dmS forPlumber
-screen -S forPlumber -p 0 -X stuff "exec ./startPlumber.R"
-echo done2
-screen -dmS forShiny
-screen -S forShiny -p 0 -X stuff "exec ./startShiny.R"
-echo done3
+screen -dmS pimpcraftPlumber
+screen -S pimpcraftPlumber -p 0 -X stuff "exec ./startPlumber.R"
+screen -dmS pimpcraftShiny
+screen -S pimpcraftShiny -p 0 -X stuff "exec ./startShiny.R"
+
+if screen -ls | grep -q pimpcraftSpigot && screen -ls | grep -q pimpcraftPlumber && screen -ls | grep -q pimpcraftShiny
+then
+  echo "PiMPCraft is up and running!"
+else
+  echo "Something went wrong. You can try starting the PiMPCraft components individually (see documentation)."
+fi
--- stop.sh
+++ stop.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-screen -S forSpigot -p 0 -X stuff "stop
+screen -S pimpcraftSpigot -p 0 -X stuff "stop
 "
 
-screen -S forPlumber -p 0 -X kill
-screen -S forShiny -p 0 -X kill
+screen -S pimpcraftPlumber -p 0 -X kill
+screen -S pimpcraftShiny -p 0 -X kill
+
+sleep 5;
+
+if ! screen -ls | grep -q pimpcraftSpigot && ! screen -ls | grep -q pimpcraftPlumber && ! screen -ls | grep -q pimpcraftShiny
+then
+  echo "PiMPCraft has stopped."
+else
+  echo "Something went wrong. Please stop the PiMPCraft screen sessions individually (see the screen documentation)."
+fi
