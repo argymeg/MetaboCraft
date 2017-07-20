76e4c856de0bdbc6eb9dc550971b255c47c88a05
argymeg <argymeg@gmail.com>
2017-07-20 12:03:31 +0100

    Script to refresh world and server config

---
+++ clearworld.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Refreshing server..."
+
+cd spigot
+rm -r world*
+
+screen -dmS initServer
+screen -S initServer -p 0 -X stuff "exec java -jar $(ls | grep spigot- )
+"
+screen -S initServer -p 0 -X stuff "gamerule doDaylightCycle false
+"
+screen -S initServer -p 0 -X stuff "gamerule doWeatherCycle false
+"
+screen -S initServer -p 0 -X stuff "time set 6000
+"
+screen -S initServer -p 0 -X stuff "stop
+"
+
+while screen -ls | grep -q initServer
+do
+  sleep 1
+done
+
+echo "Done!"
