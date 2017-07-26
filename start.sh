4f180054c8a5dc73a9f166dc0d92414d38c28171
argymeg <argymeg@gmail.com>
2017-07-26 14:48:16 +0100

    Use variables in starter and stopper

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-cd spigot
-screen -dmS pimpcraftSpigot
-screen -S pimpcraftSpigot -p 0 -X stuff "cd spigot
+spigotVer=1.11.2
+spigotFname=spigot-$spigotVer.jar
+plumberFname=startPlumber.R
+shinyFname=startShiny.R
+spigotDir=spigot
+spigScrName=pimpcraftSpigot
+plumScrName=pimpcraftPlumber
+shinScrName=pimpcraftShiny
+
+cd $spigotDir
+screen -dmS $spigScrName
+screen -S $spigScrName -p 0 -X stuff "cd spigot
 "
-screen -S pimpcraftSpigot -p 0 -X stuff "exec java -jar $(ls | grep spigot- )
+screen -S $spigScrName -p 0 -X stuff "exec java -jar $spigotFname
 "
 
 #Start plumber and shiny as background children in their own shells
@@ -12,15 +21,15 @@ screen -S pimpcraftSpigot -p 0 -X stuff "exec java -jar $(ls | grep spigot- )
 #refuse to die when their screen does) but we can't tell if they actually
 #started correctly, which is A Bad Idea(tm).
 cd ../Rscripts
-screen -dmS pimpcraftPlumber
-screen -S pimpcraftPlumber -p 0 -X stuff "./startPlumber.R &
+screen -dmS $plumScrName
+screen -S $plumScrName -p 0 -X stuff "./$plumberFname &
 "
-screen -dmS pimpcraftShiny
-screen -S pimpcraftShiny -p 0 -X stuff "./startShiny.R &
+screen -dmS $shinScrName
+screen -S $shinScrName -p 0 -X stuff "./$shinyFname &
 "
 
 sleep 5;
-if screen -ls | grep -q pimpcraftSpigot && screen -ls | grep -q pimpcraftPlumber && screen -ls | grep -q pimpcraftShiny
+if screen -ls | grep -q $spigScrName && screen -ls | grep -q $plumScrName && screen -ls | grep -q $shinScrName
 then
   printf "PiMPCraft is up and running!\nYou can now access your local PiMPCraft homepage at\nhttp://localhost:32909/\n"
 else
--- stop.sh
+++ stop.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-screen -S pimpcraftSpigot -p 0 -X stuff "stop
+spigScrName=pimpcraftSpigot
+plumScrName=pimpcraftPlumber
+shinScrName=pimpcraftShiny
+
+screen -S $spigScrName -p 0 -X stuff "stop
 "
-screen -S pimpcraftPlumber -p 0 -X stuff 'kill $! ; exit
+screen -S $plumScrName -p 0 -X stuff 'kill $! ; exit
 '
-screen -S pimpcraftShiny -p 0 -X stuff 'kill $! ; exit
+screen -S $shinScrName -p 0 -X stuff 'kill $! ; exit
 '
 
 sleep 5;
 
-if ! screen -ls | grep -q pimpcraftSpigot && ! screen -ls | grep -q pimpcraftPlumber && ! screen -ls | grep -q pimpcraftShiny
+if ! screen -ls | grep -q $spigScrName && ! screen -ls | grep -q $plumScrName && ! screen -ls | grep -q $shinScrName
 then
   echo "PiMPCraft has stopped."
 else
